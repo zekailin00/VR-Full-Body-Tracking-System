8206c32d7137a35af6a26bb1dda46a724a7ed6d8
zekailin00 <zekailin00@gmail.com>
2022-10-31 17:06:31 -0700

    algorithm IO

---
+++ software/server/algorithm/input_struct.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+head_rot = np.zeros(3)
+head_pos = np.zeros(3)
+
+left_hand_rot = np.zeros(3)
+left_hand_pos = np.zeros(3)
+
+right_hand_rot = np.zeros(3)
+right_hand_pos = np.zeros(3)
+
+imu1_acc = np.zeros(3)
+imu1_gyro = np.zeros(3)
+
+imu2_acc = np.zeros(3)
+imu2_gyro = np.zeros(3)
+
+imu3_acc = np.zeros(3)
+imu3_gyro = np.zeros(3)
+
+imu4_acc = np.zeros(3)
+imu4_gyro = np.zeros(3)
+
+imu5_acc = np.zeros(3)
+imu5_gyro = np.zeros(3)
+
+imu6_acc = np.zeros(3)
+imu6_gyro = np.zeros(3)
+
+imu7_acc = np.zeros(3)
+imu7_gyro = np.zeros(3)
+
+imu8_acc = np.zeros(3)
+imu8_gyro = np.zeros(3)
