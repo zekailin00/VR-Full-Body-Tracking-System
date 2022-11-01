9c6b7842e4e3d2273fba1ece677938bba5fdcb12
Hsiu-Ting Yeh <59615732+TsumaHibiki@users.noreply.github.com>
2022-10-31 17:09:38 -0700

    implement server to receive post data

--- software/server/app/tracker/trackerRuntime.py
+++ software/server/app/tracker/trackerRuntime.py
@@ -1,4 +1,5 @@
 import os, sys
+import json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from flask import Blueprint, current_app, session
@@ -8,3 +9,13 @@ import random
 
 
 bp = Blueprint("tracker-runtime", __name__, url_prefix="/tracker-runtime")
+
+@bp.route("/GyroAcc1", methods=["POST"])
+def unity_tracking():
+    print("received")
+    data = request.data
+   # dataDict = json.loads(data)
+   # print(dataDict)
+    print(data)
+
+    return {"retval":1 }
