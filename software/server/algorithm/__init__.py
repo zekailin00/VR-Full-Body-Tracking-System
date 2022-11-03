25ed03ef9b20f6e371270042d16092cb32c1c7be
Hsiu-Ting Yeh <59615732+TsumaHibiki@users.noreply.github.com>
2022-11-02 22:29:06 -0700

    make correction on if statement

--- software/server/algorithm/__init__.py
+++ software/server/algorithm/__init__.py
@@ -19,28 +19,28 @@ def VR_data_in(head_rot, head_pos, left_hand_rot, left_hand_pos, right_hand_rot,
     din.right_hand_pos = right_hand_pos
 
 def Sensor_data_in(imuNum, imu_accin, imu_gyroin):
-    if(imuNum == "imu1"):
+    if(imuNum == ["imu1"]):
         din.imu1_acc = imu_accin
         din.imu1_gyro = imu_gyroin
-    elif(imuNum == "imu2"):
+    elif(imuNum == ["imu2"]):
         din.imu2_acc = imu_accin
         din.imu2_gyro = imu_gyroin
-    elif(imuNum == "imu3"):
+    elif(imuNum == ["imu3"]):
         din.imu3_acc = imu_accin
         din.imu3_gyro = imu_gyroin
-    elif(imuNum == "imu4"):
+    elif(imuNum == ["imu4"]):
         din.imu4_acc = imu_accin
         din.imu4_gyro = imu_gyroin
-    elif(imuNum == "imu5"):
+    elif(imuNum == ["imu5"]):
         din.imu5_acc = imu_accin
         din.imu5_gyro = imu_gyroin
-    elif(imuNum == "imu6"):
+    elif(imuNum == ["imu6"]):
         din.imu6_acc = imu_accin
         din.imu6_gyro = imu_gyroin
-    elif(imuNum == "imu7"):
+    elif(imuNum == ["imu7"]):
         din.imu7_acc = imu_accin
         din.imu7_gyro = imu_gyroin
-    elif(imuNum == "imu8"):
+    elif(imuNum == ["imu8"]):
         din.imu8_acc = imu_accin
         din.imu8_gyro = imu_gyroin
 
@@ -59,15 +59,19 @@ def intialize():
 def thread_function(name):
     while(True):
         logging.info("Thread %s: starting", name)
-        print(buffer)
-        print(din.head_rot)
-        print(din.head_pos)
+        # print(buffer)
+        # print(din.head_rot)
+        # print(din.head_pos)
 
-        print(din.left_hand_rot)
-        print(din.left_hand_pos)
+        # print(din.left_hand_rot)
+        # print(din.left_hand_pos)
 
-        print(din.right_hand_rot)
-        print(din.right_hand_pos)
+        # print(din.right_hand_rot)
+        # print(din.right_hand_pos)
+
+        print(din.imu1_acc)
+        print(din.imu1_gyro)
+        print("\n")
 
         time.sleep(1)
         logging.info("Thread %s: finishing", name)
